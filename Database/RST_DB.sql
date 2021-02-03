9f8c6405cc9858c6c911fc6fad87a2f9c7cfaa14
bnd011 <bnd011@latech.edu>
2021-02-02 19:49:28 -0600

    Corrected MSSQL database creation file Website supports login and register on local machine server Still need to host remote SQL server and connect to backend of website

--- Database/RST_DB.sql
+++ Database/RST_DB.sql
@@ -1,19 +1,21 @@
-/*create database RST_DB;
+drop database if exists RST_DB;
+create database RST_DB;
+use RST_DB;
 drop table if exists user_login_info;
 create table [user_login_info] (
 	user_email varchar(50) not null,
-	verify	varchar(12) not null,
-	salt	varchar(65) not null,
+	verify varchar(12) not null,
+	salt varchar(65) not null,
 	primary key (user_email)
 	);
     
 drop table if exists schedules;
-create table [user_login_info] (
+create table [schedules] (
 	schedule_IDN integer not null,
 	user_email varchar(50) not null,
-	archieved bool default 0,
+	archived bit default 0,
 	url	varchar(200) not null,
-	is_recurring bool default 0,
+	is_recurring bit default 0,
     frequency integer default 0,
     want_option integer default -1,
     price_limit integer,
@@ -34,9 +36,11 @@ create table [failed_login] (
 drop table if exists admin_info;
 create table [admin_info] (
 	Admin_email	varchar(50)	not null,
-	Admin_position	varchar(25)	not null
+	Admin_position	varchar(25)	not null,
 	primary key (Admin_email)
 	);
+
+/*
 #########################################################################
 extra table if we need it in the future.
 
