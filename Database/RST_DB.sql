d6a4d3ce6f24402aa52c8d4f2327715a781e2724
cobhalla <38087230+cobhalla@users.noreply.github.com>
2021-02-11 15:03:09 -0600

    Update RST_DB.sql
    Formatted Back to MySQL format

--- Database/RST_DB.sql
+++ Database/RST_DB.sql
@@ -2,7 +2,7 @@ drop database if exists RST_DB;
 create database RST_DB;
 use RST_DB;
 drop table if exists user_login_info;
-create table [user_login_info] (
+create table user_login_info (
 	user_email varchar(50) not null,
 	verify varchar(12) not null,
 	salt varchar(65) not null,
@@ -10,7 +10,7 @@ create table [user_login_info] (
 	);
     
 drop table if exists schedules;
-create table [schedules] (
+create table schedules (
 	schedule_IDN integer not null,
 	user_email varchar(50) not null,
 	archived bit default 0,
@@ -26,7 +26,7 @@ create table [schedules] (
 	);
     
 drop table if exists failed_login;
-create table [failed_login] (
+create table failed_login (
 	user_email	varchar(50)	not null,
 	failed_num	integer default 1,
 	Time_of_try	datetime not null,
@@ -34,56 +34,9 @@ create table [failed_login] (
 	);
 
 drop table if exists admin_info;
-create table [admin_info] (
+create table admin_info (
 	Admin_email	varchar(50)	not null,
 	Admin_position	varchar(25)	not null,
 	primary key (Admin_email)
 	);
-
-/*
-#########################################################################
-extra table if we need it in the future.
-
-drop table if exists user_payment_info;
-create table [user_payment_info] (
-	Fname varchar(25) not null,
-	Lname varchar(25) not null,
-	Uname varchar(50) not null,
-	User_address varchar(75) not null,
-	Name_on_card varchar(50) not null,
-	Card_no bigint not null,
-	Expir_date date not null,
-	CVC int not null
-	);
-
-
-alter table user_login_info add verify varchar(50) not null;
-
-alter table active_schedules add ScheduleIDN int not null;
-
-alter table active_schedules add Archived Bit default 0;
-
-alter table active_schedules add Is_Recurring Bit default 0;
-
-alter table active_schedules add Frequency int default 0;
-
-alter table active_schedules add NeedWant Bit default 0;
-
-alter table active_schedules add PriceLimit int;
-
-alter table active_schedules add ExpDate date;
-
-alter table active_schedules add CurrentPrice float not null;
-
-alter table active_schedules alter column PriceLimit float;
-
-alter table admin_info drop column Admin_passwd;
-
-alter table admin_info add Admin_salt varchar(60) not null;
-
-alter table user_login_info drop column Passwd;
-################################################################
-*/
-
-
 
