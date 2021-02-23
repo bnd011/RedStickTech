880c0184020a534947c93c4257d4e043dfb3a101
mjalvidrez <mateo7502@gmail.com>
2021-02-22 20:18:37 -0600

    Added Schedule

--- ShopBot/Controllers/HomeController.cs
+++ ShopBot/Controllers/HomeController.cs
@@ -29,6 +29,11 @@ namespace ShopBot.Controllers
             return View();
         }
 
+        public IActionResult Schedule()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
