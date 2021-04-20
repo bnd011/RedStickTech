062b3a4e1065431365a23ba1f90c11181403c9d6
Andrew Hall <cobhalla@gmail.com>
2021-04-19 19:54:38 -0500

    Beta Salt and hash with .NET5 RSA

--- ShopBot/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ShopBot/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
@@ -125,11 +126,101 @@ namespace ShopBot.Areas.Identity.Pages.Account
                 return salt;
             }
 
+            //https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.rsacryptoserviceprovider?view=net-5.0
             private static string GetHash(string input)
             {
-                //implement RSA11
-                return input;
+                try
+                {
+                    //Create a UnicodeEncoder to convert between byte array and string.
+                    UnicodeEncoding ByteConverter = new UnicodeEncoding();
+
+                    //Create byte arrays to hold original, encrypted, and decrypted data.
+                    byte[] dataToEncrypt = ByteConverter.GetBytes("Data to Encrypt");
+                    byte[] encryptedData;
+                    byte[] decryptedData;
+
+                    //Create a new instance of RSACryptoServiceProvider to generate
+                    //public and private key data.
+                    using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                    {
+
+                        //Pass the data to ENCRYPT, the public key information 
+                        //(using RSACryptoServiceProvider.ExportParameters(false),
+                        //and a boolean flag specifying no OAEP padding.
+                        encryptedData = RSAEncrypt(dataToEncrypt, RSA.ExportParameters(false), false);
+                        //https://stackoverflow.com/questions/1003275/how-to-convert-utf-8-byte-to-string
+                        string encryptedString = Encoding.UTF8.GetString(encryptedData);
+                        return encryptedString;
+                    }
+                }
+                catch (ArgumentNullException)
+                {
+                    //Catch this exception in case the encryption did
+                    //not succeed.
+                    Console.WriteLine("Encryption failed.");
+                    return "default";
+                }
             }
+
+            public static byte[] RSAEncrypt(byte[] DataToEncrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+            {
+                try
+                {
+                    byte[] encryptedData;
+                    //Create a new instance of RSACryptoServiceProvider.
+                    using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                    {
+
+                        //Import the RSA Key information. This only needs
+                        //toinclude the public key information.
+                        RSA.ImportParameters(RSAKeyInfo);
+
+                        //Encrypt the passed byte array and specify OAEP padding.  
+                        //OAEP padding is only available on Microsoft Windows XP or
+                        //later.  
+                        encryptedData = RSA.Encrypt(DataToEncrypt, DoOAEPPadding);
+                    }
+                    return encryptedData;
+                }
+                //Catch and display a CryptographicException  
+                //to the console.
+                catch (CryptographicException e)
+                {
+                    Console.WriteLine(e.Message);
+
+                    return null;
+                }
+            }
+
+            public static byte[] RSADecrypt(byte[] DataToDecrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+            {
+                try
+                {
+                    byte[] decryptedData;
+                    //Create a new instance of RSACryptoServiceProvider.
+                    using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                    {
+                        //Import the RSA Key information. This needs
+                        //to include the private key information.
+                        RSA.ImportParameters(RSAKeyInfo);
+
+                        //Decrypt the passed byte array and specify OAEP padding.  
+                        //OAEP padding is only available on Microsoft Windows XP or
+                        //later.  
+                        decryptedData = RSA.Decrypt(DataToDecrypt, DoOAEPPadding);
+                    }
+                    return decryptedData;
+                }
+                //Catch and display a CryptographicException  
+                //to the console.
+                catch (CryptographicException e)
+                {
+                    Console.WriteLine(e.ToString());
+
+                    return null;
+                }
+            }
+            
         }
 
         public async Task OnGetAsync(string returnUrl = null)
