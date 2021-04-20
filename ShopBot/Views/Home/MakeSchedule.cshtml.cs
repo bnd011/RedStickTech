ef23c0cd1a75290c5af1ca33f921cd3468764551
Austin <harveymaustin@hotmail.com>
2021-04-19 22:45:55 -0500

    Actually included the files this timme

---
+++ ShopBot/Views/Home/MakeSchedule.cshtml.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShopBot.Views.Home
+{
+    public class MakeScheduleModel : PageModel
+    {
+        
+        public class InputModel
+        {
+            [Required]
+            public string ItemName { get; set; }
+            
+            [Required]
+            [DataType(DataType.Date)]
+            public string OrderDate { get; set; }
+
+            [Required]
+            
+            public int Quantity { get; set; }
+
+            
+        }
+
+        public InputModel Input { get; set; }
+
+        public void OnGet()
+        {
+            
+        }
+    }
+}
