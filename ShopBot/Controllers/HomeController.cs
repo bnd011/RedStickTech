d530a40323d6e6b37c643ee9aeae8f6b6e15c858
Andrew Hall <cobhalla@gmail.com>
2021-05-03 20:31:19 -0500

    view schedules after logging in. Quantity support added to get schedules

--- ShopBot/Controllers/HomeController.cs
+++ ShopBot/Controllers/HomeController.cs
@@ -48,11 +48,21 @@ namespace ShopBot.Controllers
 
         public IActionResult GetSchedule()
         {
+            if(accuout != null) 
+            {
+                //string encodedString = HttpUtility.HtmlEncode(scheduleString);
+                ViewBag.scheduleMessage = GetSchedules();
+                return View();
+            }
+            else
+            {
+                string scheduleString = "<h2> Login to view Schedules </h2> <table class=\"table table-striped table-bordered \"> " +
+                                                         "<thead> <tr> <th scope=\"col\"> URL </th> <th scope=\"col\"> Item </th> </tr> ";
 
-            string scheduleString = "<table> <tr> <th>User Name</th> <th>URL</th ><th>Item</th> </tr> </table>";
-            //string encodedString = HttpUtility.HtmlEncode(scheduleString);
-            ViewBag.scheduleMessage = scheduleString;
-            return View();
+                //string encodedString = HttpUtility.HtmlEncode(scheduleString);
+                ViewBag.scheduleMessage = scheduleString;
+                return View();
+            }
         }
         public IActionResult About()
         {
@@ -168,6 +178,70 @@ namespace ShopBot.Controllers
             //string encodedString = HttpUtility.HtmlEncode(scheduleString);
             ViewBag.scheduleMessage = scheduleString;
             return View("GetSchedule");
+        }
+
+        public string GetSchedules()
+        {
+            List<String[]> schedules = new();
+            Console.WriteLine(" User name: " + Account);
+            //Console.WriteLine("Must've Worked");
+            string ConnectionStr = "Server= rst-db-do-user-8696039-0.b.db.ondigitalocean.com;Port = 25060;Database=RST_DB;Uid=doadmin;Pwd=wwd0oli7w2rplovh;SslMode=Required;";
+            MySqlConnection connect = new MySqlConnection(ConnectionStr);
+            MySqlCommand getSchedule = connect.CreateCommand();
+            getSchedule.CommandText = "Select * from RST_DB.schedules where `user_email` ='" + Account + "';";
+            connect.Open();
+            try
+            {
+                bool jumper = true;
+                int count = 0;
+                MySqlDataReader connection = getSchedule.ExecuteReader();
+                connection.Read();
+                while (connection.HasRows && jumper && count < 10)
+                {
+
+                    Console.WriteLine("Rows: " + connection.HasRows + "Count: " + count);
+                    if (connection.FieldCount != 6)
+                    {
+                        connect.Close();
+                        Console.WriteLine("Wrong number of fields: " + connection.FieldCount);
+                        jumper = false;
+                    }
+                    else
+                    {
+                        int ScheduleIDN = (int)connection.GetValue(0);
+                        string User_email = (string)connection.GetValue(1);
+                        ulong Archived = (ulong)connection.GetValue(2);
+                        string URL = (string)connection.GetValue(3);
+                        string ItemText = (string)connection.GetValue(4);
+                        int Quantity = (int)connection.GetValue(5);
+
+                        string[] results = { User_email, URL, ItemText, Quantity.ToString() };
+                        Console.WriteLine("SIDN: " + ScheduleIDN + " Archived: " + Archived);
+                        Console.WriteLine(results[0] + " " + results[1] + " " + results[2] + " " + results[3]);
+                        schedules.Add(results);
+                        connection.Read();
+                    }
+                    count++;
+                }
+                if (schedules.Count == 0)
+                {
+                    Console.WriteLine("No matching Schedules!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            connect.Close();
+            string scheduleString = "<h2>" + Account + " </h2> <table class=\"table table-striped table-bordered \"> " +
+                                                     "<thead> <tr> <th scope=\"col\"> URL: </th> <th scope=\"col\"> QTY: </th> <th scope=\"col\"> Item: </th> </tr> ";
+            foreach (string[] schedule in schedules)
+            {
+                scheduleString = scheduleString + "<tr> <td>" + schedule[1] + " </td> <td>" + schedule[3] + " </td> <td>" + schedule[2] + " </td> </tr> ";
+            }
+            scheduleString = scheduleString + "</thead> </table>";
+            //string encodedString = HttpUtility.HtmlEncode(scheduleString);
+            return scheduleString;
         }
     }
 }
